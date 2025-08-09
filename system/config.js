3292fc6fa498933dff99e2983bf38077b86e1e03
Youngz <kaffabre9@gmail.com>
2025-08-09 22:32:39 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,1 +1,12 @@
-
+// Buat Nyeting
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6283****"] 
+global.namabot = 'Serah Yak'
+//======================
+global.mess = { 
+owner: "❌ 𝗔𝗞𝗦𝗘𝗦 𝗗𝗜 𝗧𝗢𝗟𝗔𝗞\nFitur ini khusus owner‼️",
+    premium: "❌ 𝗔𝗞𝗦𝗘𝗦 𝗗𝗜 𝗧𝗢𝗟𝗔𝗞\nFitur ini khusus premium‼️",
+succes: '✅ 𝙎𝙐𝘾𝘾𝙎𝙀𝙎𝙎 𝘽𝘼𝙉𝙂'
+}
+//======================
